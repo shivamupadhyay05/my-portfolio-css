04976120dd5197dc8cdcd1a71a13e04696c1e584
shivamupadhyay05 <shivamu0504@gmail.com>
2025-08-01 13:56:19 +0530

    changed contact url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => (
     <h2>Contact</h2>
     <p>Email: <a href="mailto:Shivamu0504@gmail.com">Shivamu0504@gmail.com</a></p>
     <p>Phone: <a href="tel:+916390533255">+91-6390533255</a></p>
-    <p>LinkedIn: <a href="https://linkedin.com/in/" target="_blank" rel="noopener noreferrer">linkedin.com/in/</a></p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/shivam-upadhyay-34666b277/" target="_blank" rel="noopener noreferrer">linkedin.com/in/</a></p>
     <p>GitHub: <a href="https://github.com/shivamupadhyay05" target="_blank" rel="noopener noreferrer">github.com/shivamupadhyay05</a></p>
   </section>
 );
